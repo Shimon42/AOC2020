577bcefab3998004469dec618c8336f25d6dd0e8
Siméon F <55737287+Shimon42@users.noreply.github.com>
2020-12-11 20:54:16 +0100

    Create day9.js
    Part one ok

---
+++ day9.js
@@ -0,0 +1,58 @@
+// https://adventofcode.com/2020/day/7
+let stop = false;
+
+// -- XHR to get the input
+
+function successListener() {  
+  var data = this.responseText.split("\n"); // split the rules
+    console.log(data)
+   treat_code(data);
+}
+
+function failureListener(err) {  
+  console.log('Request failed', err);  
+}
+
+var request = new XMLHttpRequest();  
+request.onload = successListener;  
+request.onerror = failureListener;  
+request.open('get', 'https://adventofcode.com/2020/day/9/input', true);  
+request.send();
+
+function is_add(nbr, head)
+{
+    let x = 0;
+    let y = 0;
+    for (x = 0; x < head.length; x++)
+    {
+        for (y = 0; y < head.length; y++)
+        {
+            if (head[x] + head[y] == nbr && x != y)
+                return (true)
+        }
+    }
+    return (false);
+}
+
+function treat_code(data)
+{
+    let clone = JSON.parse(JSON.stringify(data));
+    let head = clone.slice(0,25).map(a => parseInt(a));
+    let body = data.slice(25, data.length).map(a => parseInt(a))
+    console.log(head)
+    console.log(body)
+    for (let x = 0; x < body.length; x++)
+    {
+        let res; 
+       if ((res = is_add(body[x], head)) == false)
+       {
+            console.log("FOUND BAD")
+            console.log(body[x])
+            return (body[x])
+       }
+        head.shift()
+        head.push(body[x])
+        console.log(head)
+        console.log(res)
+    }
+}
